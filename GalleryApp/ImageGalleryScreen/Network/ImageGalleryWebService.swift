3e30ae02883a95a7bec77171c46138efc7abed37
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 16:25:43 +0300

    [ADDED]: default web service

---
+++ GalleryApp/ImageGalleryScreen/Network/ImageGalleryWebService.swift
@@ -0,0 +1,15 @@
+//
+//  ImageGalleryWebService.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import Foundation
+
+class ImageGalleryWebService: ImagesGalleryWebServiceProtocol {
+    
+    func fetchImages(completion: @escaping ([ImageResponseModel]?, FetchError?) -> Void) {
+        // TO DO: setup URLSession
+    }
+}
