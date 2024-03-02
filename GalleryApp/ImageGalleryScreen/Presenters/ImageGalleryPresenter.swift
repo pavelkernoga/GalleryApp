213f9fe0ccfabbed68244fd48c9875c99a28a3f3
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 16:27:26 +0300

    [ADDED]: presenter business logic

---
+++ GalleryApp/ImageGalleryScreen/Presenters/ImageGalleryPresenter.swift
@@ -0,0 +1,32 @@
+//
+//  ImageGalleryPresenter.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import Foundation
+
+class ImageGalleryPresenter: ImagesGalleryPresenterProtocol {
+    
+    private var webService: ImagesGalleryWebServiceProtocol
+    private weak var delegate: ImagesGalleryViewProtocol?
+    
+    required init(webService: ImagesGalleryWebServiceProtocol, delegate: ImagesGalleryViewProtocol) {
+        self.webService = webService
+        self.delegate = delegate
+    }
+    
+    func showImagesGallery() {
+        webService.fetchImages { [weak self] responseImagesModel, error in
+            if let error = error {
+                self?.delegate?.showError(error)
+                return
+            }
+            
+            if let responseImagesModel = responseImagesModel {
+                self?.delegate?.showImages(response: responseImagesModel)
+            }
+        }
+    }
+}
