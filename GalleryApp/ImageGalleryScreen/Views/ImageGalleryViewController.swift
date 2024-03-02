708beb0ee61da536ee5a574b4ecdae1ae8306ec1
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 12:25:35 +0300

    [ADDED]: Image gallery screen files structure

---
+++ GalleryApp/ImageGalleryScreen/Views/ImageGalleryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageGalleryViewController.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import UIKit
+
+class ImageGalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
