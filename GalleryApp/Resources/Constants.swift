b0f5db91c870d2d882bd3787123aaf52694ff82f
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 23:57:10 +0300

    [ADDED]: constants file

---
+++ GalleryApp/Resources/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesGalleryResources.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import Foundation
+
+struct Constants {
+    static let imagesEndPointUrlString = "https://api.unsplash.com/photos/?client_id=jW792e9RGStkjjFlC89ba4gYt5bo1bYXRs8sBdqZq5Q"
+}
