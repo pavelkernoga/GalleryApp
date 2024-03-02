b7824cfff1108570630ba5144a928e050363c702
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-03 01:18:25 +0300

    [ADDED]: fetch error cases

--- GalleryApp/ImagesGalleryScreen/Models/FetchError.swift
+++ GalleryApp/ImagesGalleryScreen/Models/FetchError.swift
@@ -10,4 +10,8 @@ import Foundation
 enum FetchError: LocalizedError {
     case invalidRequestURLString
     case failedRequest(description: String)
+    case invalidResponse
+    case invalidStatusCode(code: Int)
+    case invalidResponseModel
+    case invalidData
 }
