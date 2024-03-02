94ac2859296e845ab4e939f7f5b4f14286950b26
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 19:44:12 +0300

    [ADDED]: UIView extension

---
+++ GalleryApp/Extensions/UIView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Extension.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubviews(_ subviews: [UIView]) {
+        for subview in subviews {
+            addSubview(subview)
+        }
+    }
+}
