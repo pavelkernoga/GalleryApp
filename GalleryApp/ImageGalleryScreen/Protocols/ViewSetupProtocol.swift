3667ac1eef10052dc1c8f158605e7db878303642
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-02 16:30:51 +0300

    [ADDED]: view setup protocol

---
+++ GalleryApp/ImageGalleryScreen/Protocols/ViewSetupProtocol.swift
@@ -0,0 +1,22 @@
+//
+//  ViewSetupProtocol.swift
+//  GalleryApp
+//
+//  Created by pavel on 2.03.24.
+//
+
+import Foundation
+
+protocol ViewSetupProtocol {
+    func setupHierarchy()
+    func setupConstraints()
+    func buildView()
+}
+
+extension ViewSetupProtocol {
+
+    func buildView() {
+        setupHierarchy()
+        setupConstraints()
+    }
+}
