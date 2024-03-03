845ddc80d7028863e512a304fdbf7121c84ec31e
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-03 16:25:35 +0300

    [ADDED]: image caching logic

--- GalleryApp/ImagesGalleryScreen/Network/ImagesGalleryWebService.swift
+++ GalleryApp/ImagesGalleryScreen/Network/ImagesGalleryWebService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class ImagesGalleryWebService: ImagesGalleryWebServiceProtocol {
     // MARK: - Private properties
@@ -20,6 +21,8 @@ final class ImagesGalleryWebService: ImagesGalleryWebServiceProtocol {
         configuration.waitsForConnectivity = true
         return URLSession(configuration: configuration)
     }()
+    private let cache = NSCache<NSString, UIImage>()
+    private var responses = [URL: [(UIImage?) -> Void]]()
 
     // MARK: - Functions
     func fetchImages(completion: @escaping ([ImageItem]?, FetchError?) -> Void) {
@@ -60,5 +63,45 @@ final class ImagesGalleryWebService: ImagesGalleryWebServiceProtocol {
             }
         }
         dataTask.resume()
+    }
+
+    func getCellImage(with url: URL, completion: @escaping (UIImage?) -> Void) {
+        if let image = cache.object(forKey: url.absoluteString as NSString) {
+            debugPrint("dbg: cell image loaded from cache")
+            completion(image)
+        } else {
+            load(with: url, completion: completion)
+            debugPrint("dbg: cell image loaded from internet")
+        }
+    }
+
+    private func load(with url: URL, completion: @escaping (UIImage?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) {data, _, _ in
+
+            if self.responses[url] != nil {
+                self.responses[url]?.append(completion)
+                return
+            } else {
+                self.responses[url] = [completion]
+            }
+
+            guard let data = data,
+                  let image = UIImage(data: data),
+                  let blocks = self.responses[url] else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+
+            self.cache.setObject(image, forKey: url.absoluteString as NSString)
+
+            for block in blocks {
+                DispatchQueue.main.async {
+                    block(image)
+                }
+            }
+        }
+        task.resume()
     }
 }
