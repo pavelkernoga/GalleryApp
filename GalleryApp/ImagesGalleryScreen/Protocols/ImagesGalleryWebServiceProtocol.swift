72231545c66c38886453423026672eb40e41c01c
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-03 16:04:53 +0300

    [ADDED]: get cells images method to web service protocol

--- GalleryApp/ImagesGalleryScreen/Protocols/ImagesGalleryWebServiceProtocol.swift
+++ GalleryApp/ImagesGalleryScreen/Protocols/ImagesGalleryWebServiceProtocol.swift
@@ -5,8 +5,9 @@
 //  Created by pavel on 2.03.24.
 //
 
-import Foundation
+import UIKit
 
 protocol ImagesGalleryWebServiceProtocol {
     func fetchImages(completion: @escaping([ImageItem]?, FetchError?) -> Void)
+    func getCellImage(with url: URL, completion: @escaping (UIImage?) -> Void)
 }
