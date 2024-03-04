8de15aaa4272b4289100f9345443be3850ec59d0
Pavel Kernoha <pavel.kernoha@itechart-group.com>
2024-03-05 00:17:32 +0300

    [REFACTORED]: image item model

--- GalleryApp/ImagesGalleryScreen/Models/ImageItem.swift
+++ GalleryApp/ImagesGalleryScreen/Models/ImageItem.swift
@@ -11,9 +11,20 @@ struct ImageItem: Decodable {
     let id: String
     let description: String?
     let urls: ImageURLs
+    let title: String?
+
+    private enum CodingKeys: String, CodingKey {
+          case id, description, urls
+          case title = "alt_description"
+      }
 }
 
 struct ImageURLs: Decodable {
     let full: String
     let regular: String
+    let small: String
+
+    private enum CodingKeys: String, CodingKey {
+        case full, regular, small
+    }
 }
